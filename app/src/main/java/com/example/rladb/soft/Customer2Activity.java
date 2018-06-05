636756a1409fb7733a692244c33e712d928b5a18
yujeong <rladbwjd341@naver.com>
2018-06-05 17:51:41 +0900

    LAYOUT

--- app/src/main/java/com/example/rladb/soft/Customer2Activity.java
+++ app/src/main/java/com/example/rladb/soft/Customer2Activity.java
@@ -1,13 +1,32 @@
 package com.example.rladb.soft;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class Customer2Activity extends AppCompatActivity {
+public class Customer2Activity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button mbuttonc_PlusButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_customer2);
+
+        Button c_PlusButton= (Button) findViewById(R.id.c_PlusButton);
+        c_PlusButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.c_PlusButton:
+                Intent intentc_PlusButton = new Intent(this, TranerListActivity.class);
+                startActivity(intentc_PlusButton);
+                break;
+        }
     }
 }
---
+++ app/src/main/java/com/example/rladb/soft/TranerListActivity.java
@@ -0,0 +1,13 @@
+package com.example.rladb.soft;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TranerListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_traner_list);
+    }
+}
